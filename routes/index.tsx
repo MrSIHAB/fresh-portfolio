688abb329ca7f7864f1f35267b1903078b26ca6d
MrSIHAB <MrSIHAB@hotmail.com>
2025-07-25 19:51:05 +0600

    patch

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,7 +3,7 @@ import ProfileCard from "./(_Components)/ProfileCard.tsx";
 export default function Home() {
   return (
     <div>
-      <div class="py-20">
+      <div class="md:py-20">
         <ProfileCard />
       </div>
     </div>
