66ef63d1497723e15e1fbe7cc4101d27a7c1c210
MrSIHAB <MrSIHAB@hotmail.com>
2025-07-28 18:42:03 +0600

    Android Section

---
+++ components/HeroSection.tsx
@@ -0,0 +1,45 @@
+import { ComponentChildren } from "preact";
+import { Code, Heading } from "./Utils.tsx";
+
+interface HeroSectionOption {
+  imageUrl: string;
+  heading: string;
+  children: ComponentChildren;
+  reverse?: boolean;
+  code?: string;
+  icon?: string;
+}
+
+export default function HeroSection(
+  { imageUrl, heading, children, code, icon, reverse }: HeroSectionOption,
+) {
+  return (
+    <section
+      class={`w-full flex flex-col ${
+        reverse ? "md:flex-row-reverse" : "md:flex-row"
+      } items-center gap-8 
+    py-10 md:py-20 px-3 sm:px-8 min-h-[70vh]`}
+    >
+      {/* left: Description and code preview */}
+      <div class="flex-1 max-w-xl">
+        <Heading>
+          {icon && <img src={icon} class="max-w-10 max-h-10 inline m-2" />}
+          {heading}
+        </Heading>
+        <br />
+        {children}
+        <br />
+        <Code>{code}</Code>
+      </div>
+
+      {/* right: Flutter/Android visual */}
+      <div class="fex flex-col align-middle items-center flex-1 gap-4">
+        <img
+          src={imageUrl}
+          alt="Cryptora VPN App Screenshot"
+          class="w-full object-cover rounded-xl shadow-lg border border-primary/30 bg-black"
+        />
+      </div>
+    </section>
+  );
+}
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,6 @@
 import TechStack from "../components/TechStack.tsx";
 import ProfileCard from "./(_Components)/ProfileCard.tsx";
+import AndroidSection from "./(_Components)/Android.tsx";
 // import Project from "./(_Components)/PortfolioCard.tsx";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <ProfileCard />
       <TechStack />
+      <AndroidSection />
     </div>
   );
 }
