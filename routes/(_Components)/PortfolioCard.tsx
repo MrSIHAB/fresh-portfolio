a159648a25cb9ff42c96c84e0d2f773f0fa36f03
MrSIHAB <mrsihab@hotmail.com>
2026-01-02 22:28:40 +0600

    Fix: design of project section

--- routes/(_Components)/PortfolioCard.tsx
+++ routes/(_Components)/PortfolioCard.tsx
@@ -2,15 +2,11 @@ import ProjectIsland from "../../islands/Project.tsx";
 
 export default function Project() {
   return (
-    <div class="md:p-10" id="portfolio">
-      <div className="card portfolio-card">
-        <h2 class="text-primary mb-4 text-center">
-          <span class="text-3xl font-bold">
-            Projects
-          </span>
-        </h2>
-        <ProjectIsland />
-      </div>
+    <div class="md:p-4" id="portfolio">
+      <h2 class="text-primary mb-4 text-center text-3xl font-bold">
+        Projects
+      </h2>
+      <ProjectIsland />
     </div>
   );
 }
